80a59644a34bfe642ce9b7d76e723a3f2b0b0aef
olitank <olitank@olitank-MS-7846>
2017-06-02 18:11:34 +0200

    u

--- swing-testing-toolkit/src/main/java/xy/ui/testing/finder/DisplayedStringComponentFinder.java
+++ swing-testing-toolkit/src/main/java/xy/ui/testing/finder/DisplayedStringComponentFinder.java
@@ -6,12 +6,14 @@ import java.util.List;
 import org.apache.commons.lang3.StringEscapeUtils;
 import xy.ui.testing.Tester;
 import xy.ui.testing.editor.TestEditor;
+import xy.ui.testing.util.TestingUtils;
 import xy.ui.testing.util.ValidationError;
 
 public class DisplayedStringComponentFinder extends MatchingComponentFinder {
 	private static final long serialVersionUID = 1L;
 
 	protected String visibleString = "";
+	protected boolean visibleStringInTree = false;
 
 	public String getVisibleString() {
 		return visibleString;
@@ -21,19 +23,39 @@ public class DisplayedStringComponentFinder extends MatchingComponentFinder {
 		this.visibleString = visibleString;
 	}
 
+	public boolean isVisibleStringInTree() {
+		return visibleStringInTree;
+	}
+
+	public void setVisibleStringInTree(boolean visibleStringInTree) {
+		this.visibleStringInTree = visibleStringInTree;
+	}
+
 	@Override
 	protected boolean initializeSpecificValues(Component c, TestEditor testEditor) {
 		List<String> visibleStrings = testEditor.getTester().extractDisplayedStrings(c);
-		if (visibleStrings.size() == 0) {
-			return false;
+		if (visibleStrings.size() > 0) {
+			visibleString = visibleStrings.get(0);
+			return true;
 		}
-		visibleString = visibleStrings.get(0);
-		return true;
+		visibleStrings = TestingUtils.extractComponentTreeDisplayedStrings(c, testEditor.getTester());
+		if (visibleStrings.size() > 0) {
+			visibleString = visibleStrings.get(0);
+			visibleStringInTree = true;
+			return true;
+		}
+		return false;
 	}
 
 	@Override
 	protected boolean matchesInContainingWindow(Component c, Tester tester) {
-		List<String> visibleStrings = tester.extractDisplayedStrings(c);
+		List<String> visibleStrings;
+		if (visibleStringInTree) {
+			visibleStrings = TestingUtils.extractComponentTreeDisplayedStrings(c, tester);
+		} else {
+			visibleStrings = tester.extractDisplayedStrings(c);
+
+		}
 		for (String s : visibleStrings) {
 			if (visibleString.equals(s)) {
 				return true;
